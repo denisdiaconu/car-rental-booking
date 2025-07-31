244e724008445c6298101f3ca92d81b83b88156f
denisdiaconu <denisdiaconu117@yahoo.com>
2025-07-31 10:54:03 +0200

    Add routes for all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Cars from './pages/Cars';
+import CarDetails from './pages/CarDetails';
+import MyBookings from './pages/MyBookings';
 
 const App = () => {
-  const [ showLogin, setShowLogin] = useState(false);
-  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner');
   return (
     <>
-      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
-    </>
-  )
-}
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
