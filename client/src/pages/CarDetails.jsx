472a33df3c77e59471662c17cb6c510e938b9353
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-01 12:43:21 +0200

    Add back button & state to car details page

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { assets, dummyCarData } from '../assets/assets';
 
 const CarDetails = () => {
-  return (
-    <div>CarDetails</div>
-  )
-}
+  const [car, setCar] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-export default CarDetails
+  useEffect(() => {
+    setCar(dummyCarData.find((car) => car._id === id));
+  }, [id]);
+  return car ? (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-16 ">
+      <button
+        onClick={() => navigate(-1)}
+        className="flex items-center gap-2 mb-6 text-gray-500 cursor-pointer"
+      >
+        <img
+          src={assets.arrow_icon}
+          alt="arrow"
+          className="rotate-180 opacity-65"
+        />
+        Back to All Cars
+      </button>
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+export default CarDetails;
