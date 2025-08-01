1aa7e5c0845baf00ea0fa7c25d489a946c1325b4
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-01 12:03:04 +0200

    Add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Cars from './pages/Cars';
 import CarDetails from './pages/CarDetails';
 import MyBookings from './pages/MyBookings';
+import Footer from './components/Footer';
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/car-details/:id" element={<CarDetails />} />
         <Route path="/my-bookings" element={<MyBookings />} />
       </Routes>
+      {!isOwnerPath && <Footer />}
     </>
   );
 };
