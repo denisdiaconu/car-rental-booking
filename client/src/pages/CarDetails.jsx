6cea6787deafebd782c51d8be62563fbf1207f05
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-01 13:49:12 +0200

    Add details and form to car details page

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { assets, dummyCarData } from '../assets/assets';
+import Loader from '../components/Loader';
 
 const CarDetails = () => {
   const [car, setCar] = useState(null);
   const { id } = useParams();
   const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY;
 
   useEffect(() => {
     setCar(dummyCarData.find((car) => car._id === id));
@@ -23,9 +25,113 @@ const CarDetails = () => {
         />
         Back to All Cars
       </button>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12">
+        {/* LEFT: car img and details */}
+        <div className="lg:col-span-2">
+          <img
+            src={car.image}
+            alt="car"
+            className="w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md"
+          />
+          <div className="space-y-6">
+            <div>
+              <h1 className="text-3xl font-bold">
+                {car.brand} {car.model}
+              </h1>
+              <p className="text-gray-500 text-lg">
+                {car.category} • {car.year}
+              </p>
+            </div>
+            <hr className="border-borderColor my-6" />
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+              {[
+                {
+                  icon: assets.users_icon,
+                  text: `${car.seating_capacity} Seats`,
+                },
+                { icon: assets.fuel_icon, text: car.fuel_type },
+                { icon: assets.car_icon, text: car.transmission },
+                { icon: assets.location_icon, text: car.location },
+              ].map(({ icon, text }) => (
+                <div
+                  key={text}
+                  className="flex flex-col items-center bg-light p-4 rounded-lg"
+                >
+                  <img src={icon} alt="icon" className="h-5 mb-2" />
+                  {text}
+                </div>
+              ))}
+            </div>
+            {/* DESCRIPTION */}
+            <div>
+              <h1 className="text-xl font-medium mb-3">Description</h1>
+              <p className="text-gray-500">{car.description}</p>
+            </div>
+            {/* FEATURES */}
+            <div>
+              <h1 className="text-xl font-medium mb-3">Features</h1>
+              <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+                {[
+                  '360 Camera',
+                  'Bluetooth',
+                  'GPS',
+                  'Heated Seats',
+                  'Rear View Mirror',
+                ].map((item) => (
+                  <li key={item} className="flex items-center text-gray-500">
+                    <img
+                      src={assets.check_icon}
+                      className="h-4 mr-2"
+                      alt="check-icon"
+                    />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* RIGHT: booking form */}
+        <form className="shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500">
+          <p className="flex items-center justify-between text-2xl text-gray-800 font-semibold">
+            {currency}
+            {car.pricePerDay}
+            <span className="text-base text-gray-400 font-normal">
+              {' '}
+              per day
+            </span>
+          </p>
+          <hr className="border-borderColor my-6" />
+          <div className="flex flex-col gap-2">
+            <label htmlFor="pickup-date">Pickup Date</label>
+            <input
+              required
+              type="date"
+              className="border border-borderColor px-3 py-2 rounded-lg"
+              id="pickup-date"
+              min={new Date().toISOString().split('T')[0]}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="return-date">Return Date</label>
+            <input
+              required
+              type="date"
+              className="border border-borderColor px-3 py-2 rounded-lg"
+              id="return-date"
+            />
+          </div>
+          <button className="w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer">
+            Book Now
+          </button>
+          <p className="text-center text-sm">
+            No credit card required to reserve.
+          </p>
+        </form>
+      </div>
     </div>
   ) : (
-    <p>Loading...</p>
+    <Loader />
   );
 };
 
