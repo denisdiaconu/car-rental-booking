258673e107e8fc41a7d16935ee8c38339b48be1f
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-04 10:35:15 +0200

    Add handle submit to car details

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -9,6 +9,10 @@ const CarDetails = () => {
   const navigate = useNavigate();
   const currency = import.meta.env.VITE_CURRENCY;
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
     setCar(dummyCarData.find((car) => car._id === id));
   }, [id]);
@@ -92,7 +96,10 @@ const CarDetails = () => {
           </div>
         </div>
         {/* RIGHT: booking form */}
-        <form className="shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500">
+        <form
+          onSubmit={handleSubmit}
+          className="shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500"
+        >
           <p className="flex items-center justify-between text-2xl text-gray-800 font-semibold">
             {currency}
             {car.pricePerDay}
