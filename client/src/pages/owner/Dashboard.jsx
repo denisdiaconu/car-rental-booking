202579b4b3decadd89d92d946ce7e3de84945dbc
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-05 12:21:06 +0200

    Add booking status to dashboard

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -11,6 +11,7 @@ const Dashboard = () => {
     recentBookings: [],
     monthlyRevenue: 0,
   });
+  const currency = import.meta.env.VITE_CURRENCY
 
   const dashboardCards = [
     { title: 'Total Cars', value: data.totalCars, icon: assets.carIconColored },
@@ -34,25 +35,62 @@ const Dashboard = () => {
   useEffect(() => {
     setData(dummyDashboardData);
   }, []);
-  return <div className='px-4 pt-10 md:px-10 flex-1'>
-    <Title title="Admin Dashboard" subTitle="Monitor the overall platform performance, including total cars, bookings, revenue, and recent activities" />
-    <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl'>
-      {dashboardCards.map((card, index) => (
-        <div key={index} className='flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor'>
-          <div>
-            <h1 className='text-xs text-gray-500'>{card.title}</h1>
-            <p className='text-lg font-semibold'>{card.value}</p>
+  return (
+    <div className="px-4 pt-10 md:px-10 flex-1">
+      <Title
+        title="Admin Dashboard"
+        subTitle="Monitor the overall platform performance, including total cars, bookings, revenue, and recent activities"
+      />
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl">
+        {dashboardCards.map((card, index) => (
+          <div
+            key={index}
+            className="flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor"
+          >
+            <div>
+              <h1 className="text-xs text-gray-500">{card.title}</h1>
+              <p className="text-lg font-semibold">{card.value}</p>
+            </div>
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary/10">
+              <img src={card.icon} alt="icon" className="w-4 h-4" />
+            </div>
           </div>
-          <div className='flex items-center justify-center w-10 h-10 rounded-full bg-primary/10'>
-            <img src={card.icon} alt="icon" className='w-4 h-4' />
-          </div>
+        ))}
+      </div>
+      <div className="flex flex-wrap items-start gap-6 mb-8 w-full">
+        <div className="p-4 md:p-6 border border-borderColor rounded-md max-w-lg w-full">
+          <h1 className="text-lg font-medium">Recent Bookings</h1>
+          <p className="text-gray-500">Latest customer bookings</p>
+          {data.recentBookings.map((booking, index) => (
+            <div key={index} className="mt-4 flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <div className="hidden md:flex items-center justify-center w-12 h-12 rounded-full bg-primary/10">
+                  <img
+                    src={assets.listIconColored}
+                    alt="icon"
+                    className="h-5 w-5"
+                  />
+                </div>
+                <div>
+                  <p>
+                    {booking.car.brand} {booking.car.model}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    {booking.createdAt.split('T')[0]}
+                  </p>
+                </div>
+              </div>
+              <div className='flex items-center gap-2 font-medium'>
+                <p className='text-sm text-gray-500'>{currency}{booking.price}</p>
+                <p className='px-3 py-0.5 border border-borderColor rounded-full text-sm'>{booking.status}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+        <div></div>
+      </div>
     </div>
-    <div>
-      
-    </div>
-  </div>;
+  );
 };
 
 export default Dashboard;
