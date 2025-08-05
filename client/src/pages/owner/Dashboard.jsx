c748ef988ccd027bde7868015bc9bea45ef89c8b
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-05 11:07:55 +0200

    Add cards to dashboard

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { dummyDashboardData } from '../../assets/assets';
+import React, { useEffect, useState } from 'react';
+import { assets, dummyDashboardData } from '../../assets/assets';
+import Title from '../../components/owner/Title';
 
 const Dashboard = () => {
   const [data, setData] = useState({
@@ -11,13 +12,47 @@ const Dashboard = () => {
     monthlyRevenue: 0,
   });
 
-  useEffect(() => {
-    setData(dummyDashboardData)
-  },[])
-  return (
-    <div>
-    </div>
-  )
-}
+  const dashboardCards = [
+    { title: 'Total Cars', value: data.totalCars, icon: assets.carIconColored },
+    {
+      title: 'Total Bookings',
+      value: data.totalBookings,
+      icon: assets.listIconColored,
+    },
+    {
+      title: 'Pending',
+      value: data.pendingBookings,
+      icon: assets.cautionIconColored,
+    },
+    {
+      title: 'Confirmed',
+      value: data.completedBookings,
+      icon: assets.listIconColored,
+    },
+  ];
 
-export default Dashboard
+  useEffect(() => {
+    setData(dummyDashboardData);
+  }, []);
+  return <div className='px-4 pt-10 md:px-10 flex-1'>
+    <Title title="Admin Dashboard" subTitle="Monitor the overall platform performance, including total cars, bookings, revenue, and recent activities" />
+    <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl'>
+      {dashboardCards.map((card, index) => (
+        <div key={index} className='flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor'>
+          <div>
+            <h1 className='text-xs text-gray-500'>{card.title}</h1>
+            <p className='text-lg font-semibold'>{card.value}</p>
+          </div>
+          <div className='flex items-center justify-center w-10 h-10 rounded-full bg-primary/10'>
+            <img src={card.icon} alt="icon" className='w-4 h-4' />
+          </div>
+        </div>
+      ))}
+    </div>
+    <div>
+      
+    </div>
+  </div>;
+};
+
+export default Dashboard;
