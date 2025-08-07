aa198ba852112c553e21ac858918e6c208d8f7d3
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-07 13:56:49 +0200

    Add brand and model input to add car owner page

--- client/src/pages/owner/AddCar.jsx
+++ client/src/pages/owner/AddCar.jsx
@@ -46,6 +46,30 @@ const AddCar = () => {
           </label>
           <p className="text-sm text-gray-500">Upload a picture of your car</p>
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="flex flex-col w-full">
+            <label>Brand</label>
+            <input
+              type="text"
+              value={car.brand}
+              onChange={(e) => setCar({ ...car, brand: e.target.value })}
+              required
+              placeholder="e.g. BMW, Mercedes, Audi..."
+              className="px-3 py-2 mt-1 border border-borderColor rounded-md outline-none"
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label>Model</label>
+            <input
+              type="text"
+              value={car.model}
+              onChange={(e) => setCar({ ...car, model: e.target.value })}
+              required
+              placeholder="e.g. 320i, A4, Q5..."
+              className="px-3 py-2 mt-1 border border-borderColor rounded-md outline-none"
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
