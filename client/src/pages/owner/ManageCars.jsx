17fc43b454fef9973b751c7367549b4f8648e309
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-11 11:20:33 +0200

    Add fetch cars to manage cars

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCarData } from '../../assets/assets'
 
 const ManageCars = () => {
+  const [cars, setCars] = useState([])
+
+  const fetchOwnerCars = async () => {
+    setCars(dummyCarData)
+  }
+
+  useEffect(() => {
+    fetchOwnerCars()
+  },[])
   return (
-    <div>ManageCars</div>
+    <div>
+
+    </div>
   )
 }
 
