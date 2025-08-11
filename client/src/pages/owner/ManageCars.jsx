71c06d88f32a0afb682226e40c00a1ac87e1aeff
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-11 11:52:16 +0200

    Add table to manage cars page

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,21 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { dummyCarData } from '../../assets/assets'
+import React, { useEffect, useState } from 'react';
+import { dummyCarData } from '../../assets/assets';
+import Title from '../../components/owner/Title';
 
 const ManageCars = () => {
-  const [cars, setCars] = useState([])
+  const [cars, setCars] = useState([]);
 
   const fetchOwnerCars = async () => {
-    setCars(dummyCarData)
-  }
+    setCars(dummyCarData);
+  };
 
   useEffect(() => {
-    fetchOwnerCars()
-  },[])
+    fetchOwnerCars();
+  }, []);
   return (
-    <div>
-
+    <div className="px-4 pt-10 md:px-10 w-full">
+      <Title
+        title="Manage Your Cars"
+        subTitle="View all listed cars, update their details, or remove them from the booking platform."
+      />
+      <div className='max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6'>
+        <table className='w-full border-collapse text-left text-sm text-gray-600'>
+          <thead className='text-gray-500'>
+            <tr>
+              <th className='p-3 font-medium'>Car</th>
+              <th className='p-3 font-medium max-md:hidden'>Category</th>
+              <th className='p-3 font-medium'>Price</th>
+              <th className='p-3 font-medium max-md:hidden'>Status</th>
+              <th className='p-3 font-medium'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars.map((car, index) => (
+              <tr key={index} className='border-t border-borderColor'>
+                <td className='p-3 flex items-center gap-3'>
+                  <img src={car.image} alt={car.name} className='h-12 w-12 aspect-square rounded-md object-cover'/>
+                  <div className='max-md:hidden'>
+                    <p className='font-medium'>{car.brand} {car.model}</p>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ManageCars
+export default ManageCars;
