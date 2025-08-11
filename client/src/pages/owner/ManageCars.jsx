ac5407e89327043827cd4394c827c1541508d9ea
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-11 11:59:52 +0200

    Add car category/price to manage cars page

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -4,6 +4,7 @@ import Title from '../../components/owner/Title';
 
 const ManageCars = () => {
   const [cars, setCars] = useState([]);
+  const currency = import.meta.env.VITE_CURRENCY;
 
   const fetchOwnerCars = async () => {
     setCars(dummyCarData);
@@ -18,26 +19,39 @@ const ManageCars = () => {
         title="Manage Your Cars"
         subTitle="View all listed cars, update their details, or remove them from the booking platform."
       />
-      <div className='max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6'>
-        <table className='w-full border-collapse text-left text-sm text-gray-600'>
-          <thead className='text-gray-500'>
+      <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6">
+        <table className="w-full border-collapse text-left text-sm text-gray-600">
+          <thead className="text-gray-500">
             <tr>
-              <th className='p-3 font-medium'>Car</th>
-              <th className='p-3 font-medium max-md:hidden'>Category</th>
-              <th className='p-3 font-medium'>Price</th>
-              <th className='p-3 font-medium max-md:hidden'>Status</th>
-              <th className='p-3 font-medium'>Actions</th>
+              <th className="p-3 font-medium">Car</th>
+              <th className="p-3 font-medium max-md:hidden">Category</th>
+              <th className="p-3 font-medium">Price</th>
+              <th className="p-3 font-medium max-md:hidden">Status</th>
+              <th className="p-3 font-medium">Actions</th>
             </tr>
           </thead>
           <tbody>
             {cars.map((car, index) => (
-              <tr key={index} className='border-t border-borderColor'>
-                <td className='p-3 flex items-center gap-3'>
-                  <img src={car.image} alt={car.name} className='h-12 w-12 aspect-square rounded-md object-cover'/>
-                  <div className='max-md:hidden'>
-                    <p className='font-medium'>{car.brand} {car.model}</p>
-                    <p className='text-xs text-gray-500'>{car.seating_capacity} • {car.transmission}</p>
+              <tr key={index} className="border-t border-borderColor">
+                <td className="p-3 flex items-center gap-3">
+                  <img
+                    src={car.image}
+                    alt={car.name}
+                    className="h-12 w-12 aspect-square rounded-md object-cover"
+                  />
+                  <div className="max-md:hidden">
+                    <p className="font-medium">
+                      {car.brand} {car.model}
+                    </p>
+                    <p className="text-xs text-gray-500">
+                      {car.seating_capacity} • {car.transmission}
+                    </p>
                   </div>
+                </td>
+                <td className="p-3 max-md:hidden">{car.category}</td>
+                <td className="p-3">
+                  {currency}
+                  {car.pricePerDay}/day
                 </td>
               </tr>
             ))}
