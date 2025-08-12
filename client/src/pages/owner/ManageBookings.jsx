e7bcb6dfccfdd928ca1d06edafe2e6b91e797dfb
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-12 10:40:39 +0200

    Add table to manage bookings page

--- client/src/pages/owner/ManageBookings.jsx
+++ client/src/pages/owner/ManageBookings.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyMyBookingsData } from '../../assets/assets';
+import Title from '../../components/owner/Title';
 
 const ManageBookings = () => {
+  const [bookings, setBookings] = useState([]);
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const fetchOwnerBookings = async () => {
+    setBookings(dummyMyBookingsData);
+  }
+
+  useEffect(() => {
+    fetchOwnerBookings();
+  },[])
+
   return (
-    <div>ManageBookings</div>
+    <div className="px-4 pt-10 md:px-10 w-full">
+      <Title
+        title="Manage Bookings"
+        subTitle="Track all customer bookings, approve or cancel requests, and manage booking statuses."
+      />
+      <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6">
+        <table className="w-full border-collapse text-left text-sm text-gray-600">
+          <thead className="text-gray-500">
+            <tr>
+              <th className="p-3 font-medium">Car</th>
+              <th className="p-3 font-medium max-md:hidden">Date Range</th>
+              <th className="p-3 font-medium">Total</th>
+              <th className="p-3 font-medium max-md:hidden">Payment</th>
+              <th className="p-3 font-medium">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking, index) => (
+              <tr key={index} className="border-t border-borderColor">
+
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
