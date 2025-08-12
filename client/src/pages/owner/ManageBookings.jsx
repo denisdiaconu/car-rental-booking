d62b970c39447f52c7afa3d82f3a1c7e67f5c11d
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-12 11:19:05 +0200

    Add payment and actions to manage bookings page

--- client/src/pages/owner/ManageBookings.jsx
+++ client/src/pages/owner/ManageBookings.jsx
@@ -51,8 +51,36 @@ const ManageBookings = () => {
                   {booking.pickupDate.split('T')[0]} to{' '}
                   {booking.returnDate.split('T')[0]}
                 </td>
-                <td className='p-3'>
-                  {currency}{booking.price}
+                <td className="p-3">
+                  {currency}
+                  {booking.price}
+                </td>
+                <td className="p-3 max-md:hidden">
+                  <span className="bg-gray-100 px-3 py-1 rounded-full text-xs">
+                    offline
+                  </span>
+                </td>
+                <td className="p-3">
+                  {booking.status === 'pending' ? (
+                    <select
+                      value={booking.status}
+                      className="px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none"
+                    >
+                      <option value="pending">Pending</option>
+                      <option value="cancelled">Cancelled</option>
+                      <option value="confirmed">Confirmed</option>
+                    </select>
+                  ) : (
+                    <span
+                      className={`px-3 py-1 rounded-full text-xs font-semibold ${
+                        booking.status === 'confirmed'
+                          ? 'bg-green-100 text-green-600'
+                          : 'bg-red-100 text-red-600'
+                      }`}
+                    >
+                      {booking.status}
+                    </span>
+                  )}
                 </td>
               </tr>
             ))}
