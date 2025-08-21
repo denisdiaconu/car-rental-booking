1dbf51bb05e5c47118590561125975d0e4840e67
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-21 10:50:59 +0200

    Add user registration controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,37 @@
+import User from '../models/User';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// Generate JWT token
+const generateToken = (userId) => {
+  const payload = userId;
+  return jwt.sign(payload, process.env.JWT_SECRET);
+};
+
+// User registration controller
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password || password.length < 8) {
+      return res.json({ success: false, message: 'All fields are required' });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json({ success: false, message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const token = generateToken(user._id.toString());
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
