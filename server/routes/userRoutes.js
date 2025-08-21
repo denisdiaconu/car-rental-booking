10dc2a7fa4e62e3975b64104bc54954a27913cb9
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-21 11:01:21 +0200

    Add user register route

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { loginUser, registerUser } from '../controllers/userController';
+
+const userRouter = express.Router();
+
+userRouter.post('/register', registerUser);
