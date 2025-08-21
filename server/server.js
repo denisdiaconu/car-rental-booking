94c40ec6a1c3cdefd3360dcff0991cbbc0de2771
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-21 11:32:16 +0200

    Add user to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 // Initialize Express App
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Server on!');
 });
+app.use('/api/user', userRouter);
 
 const PORT = process.env.PORT || 3000;
 
