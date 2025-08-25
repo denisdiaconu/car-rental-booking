01e2b4f4d7b0b6d5d615d3418ccbc1d305ff8b52
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-25 11:34:22 +0200

    Add imagekit package

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -18,6 +18,8 @@ export const addCar = async (req, res) => {
         const {_id} = req.user;
         let car = JSON.parse(req.body.carData);
         const imageFile = req.file;
+
+        
     } catch(error){
         console.log(error.message);
         res.json({ success: false, message: error.message });
