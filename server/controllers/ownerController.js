31289ff47827561ccfd17848418cf1491cbb25b7
denisdiaconu <denisdiaconu117@yahoo.com>
2025-08-28 13:26:00 +0200

    Add update-image path

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -139,3 +139,35 @@ export const getDashboardData = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// API to update user image
+export const updateUserImage = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const imageFile = req.file;
+
+    // Upload image to imageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: '/users',
+    });
+    // Optimization using ImageKit URL transformations
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: '400' }, // Width
+        { quality: 'auto' }, // Auto compression
+        { format: 'webp' }, // Modern format
+      ],
+    });
+    const image = optimizedImageUrl;
+
+    await User.findByIdAndUpdate(_id, { image });
+    res.json({ success: true, message: 'User image updated successfully' });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
