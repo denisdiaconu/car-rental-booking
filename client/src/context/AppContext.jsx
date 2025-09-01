e6c03d1a3c4b3b637371d7f6f8338d5fd863fdc2
denisdiaconu <denisdiaconu117@yahoo.com>
2025-09-01 11:51:19 +0200

    Add function to fetch all cars from the server

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -34,10 +34,21 @@ export const AppProvider = ({ children }) => {
     }
   };
 
+  // Function to fetch all cars from the server
+  const fetchCars = async () => {
+    try {
+      const { data } = await axios.get('/api/user/cars');
+      data.success ? setCars(data.cars) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   // useEffect to retrieve the token from localStorage
   useEffect(() => {
     const token = localStorage.getItem('token');
     setToken(token);
+    fetchCars();
   }, []);
 
   // useEffect to fetch user data when token is available
