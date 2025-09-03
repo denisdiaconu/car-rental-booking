c7cc9804aad607f99b354fc48a799a0fecd03d2a
denisdiaconu <denisdiaconu117@yahoo.com>
2025-09-03 11:27:04 +0200

    Add fetch data function to dashboard

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { assets, dummyDashboardData } from '../../assets/assets';
+import { assets } from '../../assets/assets';
 import Title from '../../components/owner/Title';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const Dashboard = () => {
+  const { axios, isOwner, currency } = useAppContext();
   const [data, setData] = useState({
     totalCars: 0,
     totalBookings: 0,
@@ -11,7 +14,6 @@ const Dashboard = () => {
     recentBookings: [],
     monthlyRevenue: 0,
   });
-  const currency = import.meta.env.VITE_CURRENCY;
 
   const dashboardCards = [
     { title: 'Total Cars', value: data.totalCars, icon: assets.carIconColored },
@@ -32,9 +34,24 @@ const Dashboard = () => {
     },
   ];
 
+  const fetchDashboardData = async () => {
+    try {
+      const { data } = await axios.get('/api/owner/dashboard');
+      if (data.success) {
+        setData(data.dashboardData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
-    setData(dummyDashboardData);
-  }, []);
+    if (isOwner) {
+      fetchDashboardData();
+    }
+  }, [isOwner]);
   return (
     <div className="px-4 pt-10 md:px-10 flex-1">
       <Title
