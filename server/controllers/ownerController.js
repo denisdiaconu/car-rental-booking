aed87bac5b188b6f52132a3df029bec9099240e2
denisdiaconu <denisdiaconu117@yahoo.com>
2025-09-03 12:24:32 +0200

    Fix toggleCarAvailability and deleteCar in ownerController

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -65,7 +65,7 @@ export const toggleCarAvailability = async (req, res) => {
   try {
     const { _id } = req.user;
     const { carId } = req.body;
-    const car = await Car.findById({ carId });
+    const car = await Car.findById(carId);
     if (car.owner.toString() !== _id.toString()) {
       return res.json({ success: false, message: 'Unauthorized' });
     }
@@ -83,7 +83,7 @@ export const deleteCar = async (req, res) => {
   try {
     const { _id } = req.user;
     const { carId } = req.body;
-    const car = await Car.findById({ carId });
+    const car = await Car.findById(carId);
     if (car.owner.toString() !== _id.toString()) {
       return res.json({ success: false, message: 'Unauthorized' });
     }
