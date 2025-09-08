946abdfceb90270ed429b0e374bbfb2a7f2e1998
denisdiaconu <denisdiaconu117@yahoo.com>
2025-09-08 11:26:46 +0200

    Add motion animation to Hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -20,11 +20,24 @@ const Hero = () => {
     );
   };
   return (
-    <motion.div className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center">
-      <h1 className="text-4xl md:text-5xl font-semibold">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+      className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center"
+    >
+      <motion.h1
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        className="text-4xl md:text-5xl font-semibold"
+      >
         Luxury cars on rent
-      </h1>
-      <form
+      </motion.h1>
+      <motion.form
+        initial={{ scale: 0.95, opacity: 0, y: 50 }}
+        animate={{ scale: 1, opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
         onSubmit={handleSearch}
         className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]"
       >
@@ -70,16 +83,27 @@ const Hero = () => {
             />
           </div>
         </div>
-        <button className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer">
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer"
+        >
           <img
             src={assets.search_icon}
             alt="search"
             className="brightness-300"
           />
           Search
-        </button>
-      </form>
-      <img src={assets.main_car} alt="car" className="max-h-72" />
+        </motion.button>
+      </motion.form>
+      <motion.img
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.6 }}
+        src={assets.main_car}
+        alt="car"
+        className="max-h-72"
+      />
     </motion.div>
   );
 };
