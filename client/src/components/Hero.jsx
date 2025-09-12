414df46c9a8c421d2d8c95bd78e61e6dedbc14b9
denisdiaconu <denisdiaconu117@yahoo.com>
2025-09-12 12:15:44 +0200

    Fix navbar mobile size

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -41,8 +41,9 @@ const Hero = () => {
         onSubmit={handleSearch}
         className="flex flex-col md:flex-row items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]"
       >
-        <div className="flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8">
-          <div className="flex flex-col items-start gap-2">
+        <div className="flex flex-wrap gap-4 justify-center md:flex-row md:items-center md:gap-10 min-md:ml-8">
+          {/* Pickup Location */}
+          <div className="flex flex-col w-full sm:w-auto items-start gap-2">
             <select
               required
               value={pickupLocation}
@@ -59,7 +60,9 @@ const Hero = () => {
               {pickupLocation ? pickupLocation : 'Please select location'}
             </p>
           </div>
-          <div className="flex flex-col items-start gap-2">
+
+          {/* Pick-up Date */}
+          <div className="flex flex-col w-full sm:w-auto items-start gap-2">
             <label htmlFor="pickup-date">Pick-up Date</label>
             <input
               value={pickupDate}
@@ -71,7 +74,9 @@ const Hero = () => {
               className="text-sm text-gray-500"
             />
           </div>
-          <div className="flex flex-col items-start gap-2">
+
+          {/* Return Date */}
+          <div className="flex flex-col w-full sm:w-auto items-start gap-2">
             <label htmlFor="return-date">Return Date</label>
             <input
               value={returnDate}
@@ -83,10 +88,11 @@ const Hero = () => {
             />
           </div>
         </div>
+
         <motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          className="flex items-center mt-6 md:mt-0 justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer"
+          className="flex items-center mt-4 md:mt-0 justify-center gap-1 px-9 py-3 w-full sm:w-auto bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer"
         >
           <img
             src={assets.search_icon}
